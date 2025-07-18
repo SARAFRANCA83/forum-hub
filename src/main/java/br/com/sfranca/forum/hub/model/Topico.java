2b83fc0a7652dc8d39d6f382a575868b8a8d35ad
Sara França <sara.assessoria2015@gmail.com>
2025-07-18 19:19:06 -0300

    feat: implementa modelo inicial do banco com JPA e MySQ

---
+++ src/main/java/br/com/sfranca/forum/hub/model/Topico.java
@@ -0,0 +1,34 @@
+package br.com.sfranca.forum.hub.model;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String mensagem;
+
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
+
+    @ManyToOne
+    private Usuario autor;
+
+    @ManyToOne
+    private Curso curso;
+}
+
