ed4bdac432e5cb3c6e8034c6764fb987c51ae9e2
Sara França <sara.assessoria2015@gmail.com>
2025-07-19 16:04:07 -0300

    refatora TopicoResponseDTO com anotação @JsonInclude e estrutura limpa

--- src/main/java/br/com/sfranca/forum/hub/dto/TopicoResponseDTO.java
+++ src/main/java/br/com/sfranca/forum/hub/dto/TopicoResponseDTO.java
@@ -1,4 +1,16 @@
 package br.com.sfranca.forum.hub.dto;
 
-public record TopicoResponseDTO() {
-}
+import br.com.sfranca.forum.hub.model.StatusTopico;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL) // Ignora campos nulos no JSON
+public record TopicoResponseDTO(
+        String titulo,
+        String mensagem,
+        LocalDateTime dataCriacao,
+        StatusTopico status,
+        String autor,
+        String curso
+) {}
